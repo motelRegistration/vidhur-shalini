515ca0d5772d66cc5fd30139ed79921e597b896a
Vidhur Savyasachin <vidhursachin@gmail.com>
2020-02-22 01:26:34 -0500

    LoginVC Signup VC

---
+++ motel_registration/motel_registration/Controller/SignupVC.swift
@@ -0,0 +1,30 @@
+//
+//  SignupVC.swift
+//  motel_registration
+//
+//  Created by Vidhur Savyasachin on 2/22/20.
+//  Copyright © 2020 Shalini Garikapaty. All rights reserved.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
