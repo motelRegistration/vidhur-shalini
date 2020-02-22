1001bdbd853b134c13f49e0062236e3cc1937063
Shalini Garikapati <shalugarikapaty96@gmail.com>
2020-02-22 00:47:51 -0500

    happy happy

---
+++ motel_registration/motel_registration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  motel_registration
+//
+//  Created by Shalu Garikapaty on 2/22/20.
+//  Copyright © 2020 Shalini Garikapaty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
