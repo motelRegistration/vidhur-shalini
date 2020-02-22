21bb717fe8d6bc39d4dceb3d8db076f66b747514
Vidhur Savyasachin <vidhursachin@gmail.com>
2020-02-22 01:12:22 -0500

    Let's start

--- motel_registration/motel_registration/ViewController.swift
+++ motel_registration/motel_registration/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hey there")
         // Do any additional setup after loading the view.
     }
 
